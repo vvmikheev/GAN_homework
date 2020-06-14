3ac4ac5cdbf6071b00bfec47197d6319002e0321
vvmikheev <mikheev.vv@phystech.edu>
2020-06-14 09:35:10 +0300

    some mistakes are fixed

--- models.py
+++ models.py
@@ -158,7 +158,6 @@ class Discriminator(nn.Module):
         self.final = nn.Sequential(
             nn.Conv2d(256, 1, 3, padding=1),
             nn.BatchNorm2d(1),
-            nn.ReLU(),
             nn.MaxPool2d(2)    # 8 -> 4
         )
 
